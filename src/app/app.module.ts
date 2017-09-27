e011606745ae2429ec06e89cb9e984aff6bc9119
StanPri <spribytkovsky@yahoo.com>
2017-09-27 13:01:25 -0700

    HTTP to retrieve and save data added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,36 @@
-import { DashboardComponent } from 'app/dashboard.component';
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from 'app/app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
 import { RouterModule } from '@angular/router';
+import { DashboardComponent } from 'app/dashboard.component';
+import { HeroSearchComponent } from 'app/hero-search.component';
 
-import { AppRoutingModule } from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   providers: [HeroService],
